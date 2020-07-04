fae37fad98a8a3565c8d52bba662d1ad9cd2c9c2
fzngagan <fzngagan@gmail.com>
2020-07-04 13:16:25 +0530

    Improved the rating spec

--- spec/lib/ratings/rating_spec.rb
+++ spec/lib/ratings/rating_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 
 describe DiscourseRatings::Rating do
   let(:rating_hash) { JSON.parse('[{"type":"pointers","value":"4", "pavilion": "yes"}]')}
+  let(:multiple_rating_hash) { JSON.parse('[{"type":"pointers","value":"4", "pavilion": "yes"}, {"type":"handwriting","value":"3"}]')}
 
   describe "#build_list" do
     let(:rating) { DiscourseRatings::Rating.build_list(rating_hash)}
@@ -34,8 +35,18 @@ describe DiscourseRatings::Rating do
   end
 
   describe "#build_model_list" do
-    it "builds the correct list based on types available on the topic" do
-      
+    let(:rating_topic_1) { Fabricate(:topic) }
+    let(:rating_post) { Fabricate(:post, topic: rating_topic_1) }
+
+    before do
+      DiscourseRatings::Rating.build_and_set(rating_post, multiple_rating_hash)
+    end
+
+    it "builds the correct list based on types passed" do
+      rating_list = DiscourseRatings::Rating.build_model_list(rating_post.custom_fields, ["handwriting"])
+      expect(rating_list.length).to eq(1)
+      expect(rating_list[0].class).to eq(DiscourseRatings::Rating)
+      expect(rating_list[0].type).to eq("handwriting")
     end
   end
 end
