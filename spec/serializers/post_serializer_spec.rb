8ea4088fcc59aa1c8d991af3fdf70e9efec9dfe7
Faizaan Gagan <fzngagan@gmail.com>
2020-08-14 09:51:04 +0530

    FEATURE: show only current user's entries based on a site setting (#24)
    * FEATURE: show only current user's entries based on a site setting
    
    * Added tests for the functionality
    
    * Simplified the spec using stubs

---
+++ spec/serializers/post_serializer_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe PostSerializer do
+  let!(:post_rating) { { type: 'spec', value:3, weight: 1 } }
+  let!(:user1) { Fabricate(:user) }
+  let!(:rating_post) { Fabricate(:post, user: user1) }
+  let!(:user2) { Fabricate(:user) }
+  let!(:admin) { Fabricate(:admin) }
+
+  describe '#ratings' do
+    before do
+      rating_post.stubs(:ratings).returns(DiscourseRatings::Rating.build_list([post_rating]))
+      SiteSetting.rating_hide_except_own_entry = true
+    end
+    
+    it 'serializes ratings if the rating is by the author himself' do
+      serializer = PostSerializer.new(rating_post, scope: Guardian.new(user1), root: false)
+      rating_data = serializer.as_json[:ratings].as_json
+      expect(rating_data).not_to eq([])
+      expect(rating_data[0][:type]).to eq('spec')
+      expect(rating_data[0][:value]).to eq(3)
+    end
+
+    it 'doesn\'t serialize ratings if the rating is by the another user' do
+      serializer = PostSerializer.new(rating_post, scope: Guardian.new(user2), root: false)
+      expect(serializer.as_json[:ratings]).to eq(nil)
+    end
+
+    it 'bypasses the restrictions for staff/admin' do
+      serializer = PostSerializer.new(rating_post, scope: Guardian.new(admin), root: false)
+      rating_data = serializer.as_json[:ratings].as_json
+      expect(rating_data).not_to eq([])
+      expect(rating_data[0][:type]).to eq('spec')
+      expect(rating_data[0][:value]).to eq(3)
+    end
+  end
+end
