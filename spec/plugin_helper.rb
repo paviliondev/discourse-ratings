b2ee05dd7c49024db580d4fa8decb164f4a1b2fe
fzngagan <fzngagan@gmail.com>
2020-09-08 12:55:19 +0530

    added plugin_helper, formatted code

---
+++ spec/plugin_helper.rb
@@ -0,0 +1,17 @@
+require 'simplecov'
+
+SimpleCov.configure do
+  add_filter do |src|
+    src.filename !~ /discourse-ratings/ ||
+    src.filename =~ /spec/
+  end
+end
+
+RSpec.configure do |config|
+  config.around(:each) do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+      raise ActiveRecord::Rollback
+    end
+  end
+end
