699e50bf704512699c6855e57d297cac4bd8fe4a
Faizaan Gagan <fzngagan@gmail.com>
2020-11-28 17:11:44 +0530

    improved test cases for rating type admin setting

--- spec/requests/admin/rating_type_spec.rb
+++ spec/requests/admin/rating_type_spec.rb
@@ -10,16 +10,18 @@ describe DiscourseRatings::RatingTypeController do
 
     describe "#create" do
       it "creates the rating type correctly" do
-        post "/ratings/rating-type", params: create_params
+        post "/ratings/rating-type.json", params: create_params
           expect(response.status).to eq(200)
+          expect(DiscourseRatings::RatingType.all.map { |t| t.type }).to include(create_params[:type])
       end
     end
 
     describe "#update" do
       it "updates the rating type correctly" do
         DiscourseRatings::RatingType.create(create_params[:type], create_params[:name])
-          put "/ratings/rating-type/" + update_params[:type], params: update_params
+          put "/ratings/rating-type/" + update_params[:type] + ".json", params: update_params
           expect(response.status).to eq(200)
+          expect(DiscourseRatings::RatingType.get_name(create_params[:type])).to eq(update_params[:name])
       end
     end
 end
