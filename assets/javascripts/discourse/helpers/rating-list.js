6673f6ebea36a98c31be04e39de1531074154182
merefield <merefield@gmail.com>
2024-02-15 08:00:19 +0000

    COMPATIBILITY: resolve registerUnbound dreprecation

--- assets/javascripts/discourse/helpers/rating-list.js
+++ assets/javascripts/discourse/helpers/rating-list.js
@@ -1,7 +1,6 @@
-import { registerUnbound } from "discourse-common/lib/helpers";
 import { ratingListHtml } from "../lib/rating-utilities";
 import Handlebars from "handlebars";
 
-registerUnbound("rating-list", function (ratings, opts = {}) {
+export default function _ratingList(ratings, opts = {}) {
   return new Handlebars.SafeString(ratingListHtml(ratings, opts));
-});
+};
