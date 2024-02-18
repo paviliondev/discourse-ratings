a928efcc3b437242adfae9c0c123368f96ef97b9
merefield <merefield@gmail.com>
2024-02-18 20:19:57 +0000

    FIX: register raw helper

--- assets/javascripts/discourse/helpers/rating-list.js
+++ assets/javascripts/discourse/helpers/rating-list.js
@@ -1,5 +1,8 @@
+import { registerRawHelper } from "discourse-common/lib/helpers";
 import { ratingListHtml } from "../lib/rating-utilities";
 import Handlebars from "handlebars";
+
+registerRawHelper("rating-list", _ratingList);
 
 export default function _ratingList(ratings, opts = {}) {
   return new Handlebars.SafeString(ratingListHtml(ratings, opts));
