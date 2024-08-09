a2a34224ac9e29ddd3ffc311f85faaade8b9e15f
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-08-09 15:18:46 +0200

    DEV: fixes registerUnbound deprecation (#7)

--- assets/javascripts/discourse/helpers/rating-helpers.js
+++
@@ -1,7 +0,0 @@
-import Handlebars from "handlebars";
-import { registerUnbound } from "discourse-common/lib/helpers";
-import { ratingListHtml } from "../lib/rating-utilities";
-
-registerUnbound("rating-list", function (ratings, opts = {}) {
-  return new Handlebars.SafeString(ratingListHtml(ratings, opts));
-});
